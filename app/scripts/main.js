e578c393c6d87d8a8c64136d91f5218e52aeedd2
Luis Angel Marquez <luisangelmarquez@luiss-mac-pro.attlocal.net>
2015-07-20 01:47:30 -0500

    done refactoring

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,14 +9,15 @@ var gameStatus = null;
 
 // Element variables
 
-var characterOne = document.getElementById('character-one');
-var characterTwo = document.getElementById('character-two');
-var characterThree = document.getElementById('character-three');
-var characters = [characterOne, characterTwo, characterThree];
+var playerOne = document.getElementById('character-one');
+var playerTwo = document.getElementById('character-two');
+var playerThree = document.getElementById('character-three');
+var characters = [playerOne, playerTwo, playerThree];
 var playerName = document.getElementById('player-name');
 var startGameBtn = document.getElementById('start-game-button');
 var attackBtn = document.getElementById('attack-button');
 var battleInfo = document.getElementById('battle-info');
+var battleBonus = document.getElementById('battle-bonus');
 var battlePlayer = document.getElementById('battle-player');
 var battlePlayerHp = document.getElementById('player-hp');
 var battlePlayerAvatar = document.getElementById('player-avatar');
@@ -32,154 +33,157 @@ var enemies = [
 	{name: 'Mighty Centipede', avatar: 'images/enemy3.png'}
 ];
 
+console.log(enemies.sort());
+
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /* Main Objects */
 
-var Player = function(name) {
+var Character = function(type, hp) {
 	var self = this;
 	this.name = name;
-	this.hp = 500;
-	this.hpElement = battlePlayerHp;
-	this.type = null;
+	this.hp = hp;
+	this.hpElement = null;
+	this.type = type;
 	this.avatar = null;
 
 	this.setIdentity = function() {
-		for (var i = 0; i < characters.length; i++) {
-			if(selectedCharacter === characters[i]) {
-				self.type = 'player';
-				self.avatar = playerImages[i];
+		if (type == 'player') {
+			for (var i = 0; i < characters.length; i++) {
+				if(selectedCharacter === characters[i]) {
+					self.hpElement = battlePlayerHp;
+					self.type = 'player';
+					self.avatar = playerImages[i];
+					battlePlayerHp.innerHTML = self.hp;
+					battlePlayerAvatar.src = self.avatar;
+				}
+			}
+		} else if (type == 'enemy') {
+			var enemyTurn = 0;
+			var ramdonInt = getRandomInt(0, enemies.length - 1);
+			for (var i = 0; i < enemies.length; i++) {
+				if (ramdonInt === i) {
+					self.name = enemies[i].name;
+					self.hpElement = battleEnemyHp;
+					self.type == 'enemy';
+					self.avatar = enemies[i].avatar;
+					battleEnemyHp.innerHTML = self.hp;
+					battleEnemyAvatar.src = self.avatar;
+				}
 			}
 		}
 	};
-
-	this.setIdentity();
-		console.log(this.hp);
-
-};
-
-var Enemy = function() {
-	var self = this;
-	this.name = '';
-	this.hp = 100;
-	this.hpElement = battleEnemyHp;
-	this.type = 'enemy';
-	this.avatar = null;
-
-	this.setIdentity = function() {
-		var ramdonInt = getRandomInt(0, enemies.length - 1);
-		for (var i = 0; i < enemies.length; i++) {
-			if (ramdonInt === i) {
-				self.name = enemies[i].name;
-				self.avatar = enemies[i].avatar;
-			}
-		}
-	};
-
-	this.setIdentity();
-};
-
-var Attack = function() {
-	var self = this;
 
 	this.generateAttackPower = function() {
 		var ramdonInt = getRandomInt(10, 30);
 		return ramdonInt;
 	};
 
-	this.processAtack = function(attacker, victim) {
-		var ramdonHit = getRandomInt(1, 3);
-		if (ramdonHit === 3) {
-			battleInfo.innerHTML = attacker.name + ' attempted an attack against ' + victim.name + ' but missed';
-		} else {
-			victim.hp -= self.generateAttackPower();
-			battleInfo.innerHTML = attacker.name + ' attacked ' + victim.name + ' for ' + self.generateAttackPower() + ' attack power';
-			victim.hpElement.innerHTML = victim.hp;
-		}
+	this.displayHealth = function() {
+		self.hpElement.innerHTML = self.hp;
 	};
-}
+
+	this.setIdentity();
+
+	this.healthBonus = function(hpBonus) {
+		var x = getRandomInt(1,2);
+		self.hp += hpBonus;
+	};
+};
 
 /* Main Functions */
 
-var Game = function(player, enemy) {
+var Game = function() {
 	var self = this;
-	this.player = player;
-	this.enemy = enemy;
+	player = new Character('player', 400);
+	player.name = playerName.value;
+	enemy = new Character('enemy', 100);
+	gameStatus = true;
 
-	this.generatePlayer= function() {
-		battlePlayerAvatar.src = player.avatar;
-		battlePlayerHp.innerHTML = player.hp;
+	this.processAttack = function(attacker, victim) {
+		var ramdonMissed = getRandomInt(1, 3);
+
+		if (ramdonMissed == 3) {
+			battleInfo.innerHTML = attacker.name + ' missed ' + victim.name;
+		} else {
+			var ramdonAttackPower = attacker.generateAttackPower();
+			victim.hp -= ramdonAttackPower;
+			battleInfo.innerHTML = attacker.name + ' attacked ' + victim.name + ' for ' + ramdonAttackPower + ' attack power';
+			victim.displayHealth();
+		}
+
+		self.checkHealth();
 	};
 
-	this.generateEnemy= function() {
-		battleEnemyAvatar.src = enemy.avatar;
-		battleEnemyHp.innerHTML = enemy.hp;
+	this.checkHealth = function() {
+		if (enemy.hp <= 0) {
+			battleInfo.innerHTML = player.name + ' killed ' + enemy.name;
+			self.ramdonHpBonus();
+			enemy = new Character('enemy', 100);
+		} else if (player.hp <= 0) {
+			battleInfo.innerHTML = enemy.name + ' killed ' + player.name;
+			player.hp = 0;
+			player.displayHealth();
+			attackBtn.disabled = true;
+		}
 	};
 
-	this.generatePlayer();
-	this.generateEnemy();
+	this.ramdonHpBonus = function() {
+		var x = getRandomInt(1, 2);
+
+		if(x === 1) {
+			var hpBonus = 100;
+			player.healthBonus(hpBonus);
+			battleBonus.innerHTML = player.name + ' received an ' + hpBonus + ' hp bonus';
+			player.displayHealth();
+			setTimeout(function(){
+				battleBonus.innerHTML = '';
+			}, 3000);
+		}
+
+		console.log(x);
+	};
 };
 
 var initGame = function() {
-	player = new Player(playerName.value);
-	enemy = new Enemy();
-	game = new Game(player, enemy);
-	game.prototype = new Attack();
-	gameStatus = true;
-
-	this.checkGameStatus = function() {
-		if (player.hp <= 0) {
-			battleInfo.innerHTML = enemy.name + ' killed ' + player.name;
-			gameStatus = false;
-		} else if (enemy.hp <= 0) {
-			battleInfo.innerHTML = player.name + ' killed ' + enemy.name;
-			gameStatus = true;
-			enemy = new Enemy();
-			console.log(game.generateEnemy());
-		}
-	};
-	
-	this.checkGameStatus();
-}
-
-
+	game = new Game();
+};
 
 /* Event listeners*/
 
-characterOne.addEventListener('click', function(){
-	selectedCharacter = characterOne;
-	characterOne.className = "col-3 selection-character--character active";
-	characterTwo.className = "col-3 selection-character--character";
-	characterThree.className = "col-3 selection-character--character";
+playerOne.addEventListener('click', function(){
+	selectedCharacter = playerOne;
+	playerOne.className = "col-3 selection-character--character active";
+	playerTwo.className = "col-3 selection-character--character";
+	playerThree.className = "col-3 selection-character--character";
 }, false);
 
-characterTwo.addEventListener('click', function(){
-	selectedCharacter = characterTwo;
-	characterOne.className = "col-3 selection-character--character";
-	characterTwo.className = "col-3 selection-character--character active";
-	characterThree.className = "col-3 selection-character--character";
+playerTwo.addEventListener('click', function(){
+	selectedCharacter = playerTwo;
+	playerOne.className = "col-3 selection-character--character";
+	playerTwo.className = "col-3 selection-character--character active";
+	playerThree.className = "col-3 selection-character--character";
 }, false);
 
-characterThree.addEventListener('click', function(){
-	selectedCharacter = characterThree;
-	characterOne.className = "col-3 selection-character--character";
-	characterTwo.className = "col-3 selection-character--character";
-	characterThree.className = "col-3 selection-character--character active";
+playerThree.addEventListener('click', function(){
+	selectedCharacter = playerThree;
+	playerOne.className = "col-3 selection-character--character";
+	playerTwo.className = "col-3 selection-character--character";
+	playerThree.className = "col-3 selection-character--character active";
 }, false);
 
 startGameBtn.addEventListener('click', function(){
 	initGame();
+	console.log(typeof player.hp);
 });
 
 attackBtn.addEventListener('click', function(){
-	if(gameStatus) {
-		game.prototype.processAtack(player, enemy);
-		checkGameStatus();
-		setTimeout(function(){
-			game.prototype.processAtack(enemy, player);
-			checkGameStatus();
-		}, 1000);
-	}
+	game.processAttack(player, enemy);
+	attackBtn.disabled = true;
+	setTimeout(function(){
+		game.processAttack(enemy, player);
+		attackBtn.disabled = false;
+	}, 1000);
 });
